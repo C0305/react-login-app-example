0acec84b650c0bc202a4cfc15946a7bbd4766a6a
Ernesto Cobos <ernesto@cobos.io>
2020-07-24 01:47:20 -0500

    I Created the scaffolding
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import "./scss/index.scss"
+import App from "./app"
+
+
+const wrapper = document.getElementById("root")
+ReactDOM.render(<App />, wrapper)
