3c127d50007841a26df30c1db8ab29aacfcb5542
Ernesto Cobos <ernesto@cobos.io>
2020-07-26 00:55:32 -0500

    Added thunk and saveToLocalStorage to middleware
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,13 @@ import {applyMiddleware, createStore} from "redux"
 const initialState = (localStorage['redux-store']) ?
     JSON.parse(localStorage['redux-store']) : {}
 
+const saveToLocalStorage = store => next => action => {
+    next(action)
+    const state = store.getState()
+    localStorage.setItem('redux-store', JSON.stringify(state))
+
+}
+
 export default () => {
-    return applyMiddleware(thunk)(createStore)(appReducer, initialState)
+    return applyMiddleware(thunk, saveToLocalStorage)(createStore)(appReducer, initialState)
 }
