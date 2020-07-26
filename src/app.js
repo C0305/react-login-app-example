0394f37b71034e76a8aa07a40ee4b152f5dd3374
Ernesto Cobos <ernesto@cobos.io>
2020-07-26 00:58:38 -0500

    Added
    Signed-off-by: Ernesto Cobos <ernesto@cobos.io>

--- src/app.js
+++ src/app.js
@@ -1,19 +1,70 @@
 import React from 'react';
 import Login from "./views/login/index";
 import Layout from "./components/layout/index";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Home from "./views/home";
+import {useSelector} from "react-redux";
 
 function App() {
     return (
         <Router>
             <Layout>
                 <Switch>
-                    <Route path="/">
+                    <LogInRoute exact path="/">
                         <Login/>
-                    </Route>
+                    </LogInRoute>
+                    <PrivateRoute exact path="/home">
+                        <Home/>
+                    </PrivateRoute>
                 </Switch>
             </Layout>
         </Router>
+    );
+}
+
+// A wrapper for <Route> that redirects to the login
+// screen if you're not yet authenticated.
+
+
+function LogInRoute({children, ...rest}) {
+    const loggedUser = useSelector(state => state.login);
+    return (
+        <Route
+            {...rest}
+            render={({location}) =>
+                !loggedUser ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/home",
+                            state: {from: location}
+                        }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+function PrivateRoute({children, ...rest}) {
+    const loggedUser = useSelector(state => state.login);
+    return (
+        <Route
+            {...rest}
+            render={({location}) =>
+                loggedUser ? (
+                    children
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: "/",
+                            state: {from: location}
+                        }}
+                    />
+                )
+            }
+        />
     );
 }
 
